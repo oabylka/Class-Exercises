12208695d4fede0f0c8c36ccedf91999d21e3836
Olie Bylka <oabylova@gmail.com>
2017-03-09 21:20:12 -0800

    Commiting change to contacts app to add form to create new contacts

--- contacts-app/app/controllers/contacts_controller.rb
+++ contacts-app/app/controllers/contacts_controller.rb
@@ -6,4 +6,17 @@ class ContactsController < ApplicationController
 	def view_all
 		@contacts = Contact.all
 	end
+
+	def new_contact_form
+
+	end
+
+	def create_contact
+		input_first_name = params[:first_name]
+		input_last_name = params[:last_name]
+		input_email = params[:email]
+		input_phone = params[:phone]
+
+		@contact = Contact.create(first_name: input_first_name, last_name: input_last_name, email: input_email, phone: input_phone)
+	end
 end
--- contacts-app/config/routes.rb
+++ contacts-app/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/view_contact" => "contacts#view_contact"
   get "/view_all" => "contacts#view_all"
+
+  get "/new_contact_form" => "contacts#new_contact_form"
+  post "/create_contact" => "contacts#create_contact"
 end
