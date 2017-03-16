4cac67476dcb47c9c9a4123c3a294416e4ab2aa0
Olie Bylka <oabylova@gmail.com>
2017-03-15 19:15:12 -0700

    Added a column middle name, and editied appropriate views, controllers, and models to reflect the change

--- contacts-app/app/controllers/contacts_controller.rb
+++ contacts-app/app/controllers/contacts_controller.rb
@@ -14,11 +14,12 @@ class ContactsController < ApplicationController
 
 	def create
 		input_first_name = params[:first_name]
+		input_middle_name = params[:middle_name]
 		input_last_name = params[:last_name]
 		input_email = params[:email]
 		input_phone = params[:phone]
 
-		@contact = Contact.create(first_name: input_first_name, last_name: input_last_name, email: input_email, phone: input_phone)
+		@contact = Contact.create(first_name: input_first_name, middle_name: input_middle_name, last_name: input_last_name, email: input_email, phone: input_phone)
 	end
 
 	def edit
@@ -28,13 +29,14 @@ class ContactsController < ApplicationController
 
 	def update
 		input_first_name = params[:first_name]
+		input_middle_name = params[:middle_name]
 		input_last_name = params[:last_name]
 		input_email = params[:email]
 		input_phone = params[:phone]
 
 		contact = Contact.find_by(id: params[:id])
 		puts contact
-		@contact = contact.update(first_name: input_first_name, last_name: input_last_name, email: input_email, phone: input_phone)
+		@contact = contact.update(first_name: input_first_name, middle_name: input_middle_name, last_name: input_last_name, email: input_email, phone: input_phone)
 		redirect_to "/contacts/#{contact.id}"
 	end
 
--- contacts-app/app/models/contact.rb
+++ contacts-app/app/models/contact.rb
@@ -5,15 +5,22 @@ class Contact < ApplicationRecord
 	end
 
 	def full_name
-		first_name+" "+last_name
+		first_name+" "+middle_name+" "+last_name
 	end
 
 	def add_prefix
 		"+81 "+phone.to_s
 	end
 
-	def find_john
-		@keyword = "John"
-		Contact.where("first_name LIKE ?", @keyword)
-	end
+#Class method vs. instance methods
+	# def self.find_john
+	# 	contacts = Contacts.all
+	# 	johns = []
+	# 	@keyword = "John"
+	# 	Contact.where("first_name LIKE ?", @keyword)
+	# 	if contact.first_name == "John"
+	# 		johns << contact
+	# 	end
+	# 	return johns
+	# end
 end
