24cc4fd760545ac3cb8ec5a8313954efee249b1b
Olie Bylka <oabylova@gmail.com>
2017-04-02 11:55:01 -0700

    Edited sfpd to be a forecasting using yahoo weather api

--- sfpd-incidents/app/controllers/incidents_controller.rb
+++ sfpd-incidents/app/controllers/incidents_controller.rb
@@ -1,6 +1,20 @@
 class IncidentsController < ApplicationController
 
 	def index
-		@incidents = Unirest.get("https://data.sfgov.org/resource/cuks-n6tp.json").body
+		if params[:city] && params[:state]
+			city = params[:city]
+			state = params[:state]
+		else
+			city = "lodi"
+			state = "ca"
+		end
+
+		@weather = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%22#{city},%20#{state}%22)&format=json").body
+		@current_conditions = @weather["query"]["results"]["channel"]["item"]["condition"]
+		@five_forecast = @weather["query"]["results"]["channel"]["item"]["forecast"]
+	end
+
+	def custom_forecast
+		render "index.html.erb"
 	end
 end
